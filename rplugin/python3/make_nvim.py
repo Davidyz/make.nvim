db8c017ba9af55a1f18817ed6f73ff448ec446f6
Zhe Yu <zcabzyu@ucl.ac.uk>
2022-03-29 08:41:13 +0100

    Run make in a new tab so that it does not interfere with the existing tab.

--- rplugin/python3/make_nvim.py
+++ rplugin/python3/make_nvim.py
@@ -16,6 +16,9 @@ class Main:
 
     @neovim.function("MakeCompletion", sync=True)
     def make_completion(self, args):
+        """
+        Auto-completion function for :Make.
+        """
         return self.run_completion(args, self.find_make(False))
 
     @neovim.command(
@@ -26,10 +29,16 @@ class Main:
         complete="customlist,MakeCompletion",
     )
     def make(self, args: List[str], range: List[List[int]]):
+        """
+        Run make command from the Makefile in the closest parent directory.
+        """
         self.run_make(self.find_make(top=False), args)
 
     @neovim.function("MakeRootCompletion", sync=True)
     def make_root_completion(self, args):
+        """
+        Auto-completion function for :MakeRoot.
+        """
         return self.run_completion(args, self.find_make(top=True))
 
     @neovim.command(
@@ -40,21 +49,32 @@ class Main:
         complete="customlist,MakeRootCompletion",
     )
     def make_root(self, args: List[str], range: List[List[int]]):
+        """
+        Run make command from the Makefile in the top-most parent directory.
+        Useful when there are multiple Makefiles in a project.
+        """
         self.run_make(self.find_make(top=True), args)
 
     def run_make(self, make_dir: Optional[str], args: List[str]) -> None:
+        """
+        Run the Makefile in the given directory.
+        """
         if make_dir is None:
             self.vim.command("echo 'No Makefile can be found.'")
             return
 
+        self.vim.command("tabnew")
         original_dir = os.getcwd()
         self.vim.command(f"cd {make_dir}")
         command = f'make {" ".join(args)}'
         self.vim.command(f'echo "{command}"')
-        self.vim.command(command)
+        self.vim.command(f"ter {command}")
         self.vim.command(f"cd {original_dir}")
 
     def run_completion(self, args: List, make_dir: Optional[str]) -> List[str]:
+        """
+        Find all matching words for the given input.
+        """
         argLead = args[0]
         self.vim.api.command(f'echo "{args}"')
 
@@ -70,7 +90,11 @@ class Main:
         else:
             return self.get_make_targets(make_dir)
 
-    def find_make(self, top=False) -> Optional[str]:
+    def find_make(self, top: bool = False) -> Optional[str]:
+        """
+        Find the directory containing the suitable Makefile.
+        top: if True, return the top-most directory for :MakeRoot.
+            Otherwise return the closest parent directory containing Makefile."""
         current_dir = os.getcwd()
         make_dir = None
         while current_dir:
@@ -88,6 +112,9 @@ class Main:
         return None if make_dir is None else os.path.realpath(make_dir)
 
     def get_make_targets(self, make_dir: Optional[str]) -> List[str]:
+        """
+        Parse a Makefile to get all the make targets for completion function.
+        """
         if make_dir is None:
             return []
         with open(os.path.join(make_dir, "Makefile")) as fin:
